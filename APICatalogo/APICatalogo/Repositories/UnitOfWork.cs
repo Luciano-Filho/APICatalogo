0af64ed3d7748f1260536383e7b5ca6b2b1e3db2
Luciano <luciano.lvbf27@hotmail.com>
2025-04-28 21:34:06 -0300

    adicionando unitOfWork

---
+++ APICatalogo/APICatalogo/Repositories/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using APICatalogo.Context;
+
+namespace APICatalogo.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private IProdutoRepository _repoProduto;
+    private ICategoriaRepository _repoCategoria;
+    public AppDbContext _context;
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+    public IProdutoRepository ProdutoRepository
+    {
+        get
+        {
+            return _repoProduto = _repoProduto ?? new ProdutoRepository(_context);
+        }
+    }
+
+    public ICategoriaRepository CategoriaRepository
+    {
+        get
+        {
+            return _repoCategoria = _repoCategoria ?? new CategoriaRepository(_context);
+        }
+    }
+
+    public void Commit()
+    {
+        _context.SaveChanges();
+    }
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+}
